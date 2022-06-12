5b6ee79ef99d8a4754b861be56e8b16690604612
Diego Carrillo <80798658+Carro1331@users.noreply.github.com>
2022-06-12 17:43:51 -0400

    Update mains().py

--- mains().py
+++ mains().py
@@ -9,3 +9,14 @@ def main():
             print('Caracter:',i," - " , hash[i], "Unicode: ",ord(hash[i])  )
         if(i == 53 ):
              print('Caracter:',i," - " , hash[i], "Unicode: ",ord(hash[i])  )
+
+def main():
+    string = "hola"
+    writePass(string)
+    hash = encode(string)
+    print(hash)
+
+    for i in range(0,len(hash)):
+        print(i ,".-" , hash[i],"\n")
+
+              
