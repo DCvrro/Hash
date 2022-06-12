3b2925aaebc0387f21f58e389ca48f97b91172bd
Diego Carrillo <80798658+Carro1331@users.noreply.github.com>
2022-06-12 17:40:52 -0400

    Se guardan los mains de pruebas

---
+++ mains().py
@@ -0,0 +1,11 @@
+def main():
+    string = "hola"
+    writePass(string)
+    hash = encode(string)
+    print(hash)
+
+    for i in range(0,len(hash)):
+        if(i == 51 ):
+            print('Caracter:',i," - " , hash[i], "Unicode: ",ord(hash[i])  )
+        if(i == 53 ):
+             print('Caracter:',i," - " , hash[i], "Unicode: ",ord(hash[i])  )
