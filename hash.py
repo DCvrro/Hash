f58f96893a1a8f075a997eabf3ab8628084d8d27
Diego Carrillo <80798658+Carro1331@users.noreply.github.com>
2022-06-12 18:16:38 -0400

    Actualizacion
    Terminado hito 1 y 2

--- hash.py
+++ hash.py
@@ -1,4 +1,3 @@
-
 def getUnicode(): # type 0 if pass without has || type 1 for hash.  
     arch = open('unicode.txt','w')
     x = 0
@@ -16,46 +15,42 @@ def getASCII(symbol):
 
 def encode(string):
     size = int(len(string))
+
+    #Listado de caracteres que no es reconocido por vsc o python. SE PROBÓ UNO POR UNO
     Null=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,
-        28,29,30,31,32,33,34,129,143,146,147,148,149,150,151,152,153,154,155,156,157,158
+        28,29,30,31,32,33,34,60,127,128,129,143,140,145,146,147,148,149,150,151,152,153,154,155,156,157,158
         ,159,160,161,162,175,183,186,217,305,385,390,399,424,425,441,446,447,450,453]
-    final = ""
+    hash = ""
     cont = 0
     if(size < 55):
         #Veamos cuanto falta para rellenar el hash
         rest = 55 - size
         for i in range(size-1): 
-            cont = ord(string[i]) +ord(string[i+1])
-            if(cont > 800): #Investigar esto.
-                cont = cont - 800 
+            cont = getASCII(string[i]) +getASCII(string[i+1]) 
+            #Al ir sumando los valores dentro del valor unicode hacemos que no se vaya escrbiiendo
+            #de manera texto plano
             
             while(cont in Null): 
                 cont= cont + (int(cont/7))
-                if(cont>800):
-                    cont = cont - 800
-            
-            final = final + chr(cont) #Con esto pasamos de unicode a caracter. 
-        cum = ord(string[size-1])
+            hash = hash + chr(cont) #Con esto pasamos de unicode a caracter. 
+        cum = getASCII(string[size-1])
     
         #Otro ciclo para rellenar
         for i in range(rest+1): #Rellenamos segun lo faltante.
             if(i%2==0):
                 cum = cum + 6
-                if(cont> 800):
-                    cont = cont - 800
-                
                 while(cum in Null):
                     cum = cum + (int(cum/7))
-                final= final + chr(cum)
+                hash= hash + chr(cum)
 
             else:
                 cum = cum - 5
                 if(cont <= 0):
-                    cont = cont + 800
+                    cont = cont + 500
 
                 while(cum in Null):
                     cum = cum + (int(cum/7))
-                final = final + chr(cum)
+                hash = hash + chr(cum)
     else: #Revisamos el caso mayor a 55
         #Si es mas largo, trabajamos en partes iguales de la misma cantidad 
         #permitida a la del hash 55.
@@ -68,35 +63,42 @@ def encode(string):
                 if(string[i]==' ' or string[i+1]==' '):
                     cont =7+size
                 else:
-                    cont = ord(string[i])+ord(string[i+1])
-                if(cont > 800):
-                    cont = cont - 800
+                    cont = getASCII(string[i])+getASCII(string[i+1])
                 if(k == parts):
                     while(cont in Null):
                         cont = cont + (int(cont/7)) 
-                    final = final + chr(cont)
+                    hash = hash + chr(cont)
                     cont = 0 
                     k = 0
                 k = k + 1
             else:
-                cont = ord(string[i]) + int(ord(string[i+1])) - int(ord(string[i+1])/2)
-                if (cont > 800):
-                    cont = cont - 800
+                cont = getASCII(string[i]) + int(getASCII(string[i+1])) - int(getASCII(string[i+1])/2)
                 while(cont in Null): 
                     cont = cont + (int(cont/7))
-        final = final + chr(cont)
-    return final
+        hash = hash + chr(cont)
+    return hash
+
+
+
 
 def writePass(text): # type 0 if pass without has || type 1 for hash.  
-    arch = open('Resultados.txt','a')
-    arch.write('Contraseña: '+ text + '\n')
-    hash = encode(text)
-    arch.write('\t'+'Hash: '+ hash  + '\n' )
-    arch.close()
+    with open('Resultados.txt','a', encoding="utf-8") as arch:
+        arch.write('Contraseña: '+ text + '\n')
+        hash = encode(text)
+        arch.write('\t'+'Hash: '+ hash  + '\n' )
+        arch.close()
 
 
 def main():
-    print("Hola")
-    string = input("Ingrese string\n")
-    writePass(string)
+    
+    val = True
+    while(val):
+        entrada = input("Ingrese palabra: ")
+        if entrada == 0:
+            break
+        writePass(entrada)
+
+
+
+
 main()
